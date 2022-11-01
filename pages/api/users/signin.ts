7ccf8f12e161c6993f8df1b2bdd668420996c55a
zkharov <zakharovvadzim@yandex.by>
2022-11-01 15:53:56 +0300

    FIX

--- pages/api/users/signin.ts
+++ pages/api/users/signin.ts
@@ -9,64 +9,70 @@ import { UserRepository } from '~/lib/repo/user-repository'
 import { AuthService } from '~/lib/auth/auth-service'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-    const { email, password } = req.body
+    try {
+        const { email, password } = req.body
 
-    if (!email || !password) {
-        return res.status(400).json({ message: 'Invalid request' })
-    }
-    if (!validateEmail(email)) {
-        return res.status(400).json({ message: 'Invalid request' })
-    }
+        if (!email || !password) {
+            return res.status(400).json({ message: 'Invalid request' })
+        }
+        if (!validateEmail(email)) {
+            return res.status(400).json({ message: 'Invalid request' })
+        }
 
-    const user = await UserRepository.findBy(email, 'email')
+        const user = await UserRepository.findBy(email, 'email')
 
-    if (!user) {
-        return res.status(404).json({ message: 'User are not exists' })
-    }
+        if (!user) {
+            return res.status(404).json({ message: 'User are not exists' })
+        }
 
-    const isPasswordCorrect = await compare(password, user.password)
+        const isPasswordCorrect = await compare(password, user.password)
 
-    if (!isPasswordCorrect) {
-        return res.status(404).json({ message: 'Password is not correct' })
-    }
+        if (!isPasswordCorrect) {
+            return res.status(404).json({ message: 'Password is not correct' })
+        }
 
-    if (user.twoFA) {
-        const otpSecret = generateSecret()
+        if (user.twoFA) {
+            const otpSecret = generateSecret()
 
-        await UserRepository.update(user.id, { otpSecret })
+            await UserRepository.update(user.id, { otpSecret })
 
-        const otpToken = otp.generate(otpSecret)
+            const otpToken = otp.generate(otpSecret)
 
-        mailer.post('send', { version: 'v3.1' }).request({
-            Messages: [
-                {
-                    From: {
-                        Email: 'zakharovvadzim@gmail.com',
-                        Name: 'Two-Factor Next App',
-                    },
-                    To: [
-                        {
-                            Email: user.email,
+            mailer.post('send', { version: 'v3.1' }).request({
+                Messages: [
+                    {
+                        From: {
+                            Email: 'zakharovvadzim@gmail.com',
+                            Name: 'Two-Factor Next App',
                         },
-                    ],
-                    Subject: 'OTP',
-                    TextPart: `OTP: ${otpToken}`,
-                    HTMLPart: `<div>OTP: </div><code>${otpToken}</code>`,
-                },
-            ],
-        })
+                        To: [
+                            {
+                                Email: user.email,
+                            },
+                        ],
+                        Subject: 'OTP',
+                        TextPart: `OTP: ${otpToken}`,
+                        HTMLPart: `<div>OTP: </div><code>${otpToken}</code>`,
+                    },
+                ],
+            })
+
+            return res.status(201).json({
+                id: user.id,
+                twoFA: true,
+                message: 'Otp is sent to email',
+            })
+        }
+
+        const jwtCookie = await AuthService.bakeJwt(user)
 
         return res
-            .status(201)
-            .json({ id: user.id, twoFA: true, message: 'Otp is sent to email' })
+            .setHeader('Set-Cookie', jwtCookie)
+            .status(200)
+            .json({ message: 'Success' })
+    } catch (err) {
+        return res.status(500).json({ err })
     }
-
-    const jwtCookie = await AuthService.bakeJwt(user)
-
-    return res
-        .setHeader('Set-Cookie', jwtCookie)
-        .status(200)
-        .json({ message: 'Success' })
 }
 
 export default handler
